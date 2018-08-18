c196dd5822329aa15b5ac82ebe14e0ca70bf770b
eveey connolly <eveey.connolly@my.jcu.edu.au>
2018-08-18 12:41:10 +1000

    add comments in regards to practical

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] will return 3
+# numbers[-1] will return 2
+# numbers[3] will return 1
+# numbers[:-1] will return [3,1,4,1,5,9]
+# numbers[3:4] will return 1
+# 5 in numbers will return True
+# 7 in numbers will return False
+# '3' in numbers will return False
+# numbers + [6, 5, 3] will return [3,1,4,1,5,9,2,6,5,3]
+
+# To change the first element of numbers to "ten": numbers[0] = "10"
+# To change the last element of numbers to 1: numbers[-1} = 1
+# To get all the elements from numbers expect the first two: numbers[2:]
+# To check if 9 is an element of numbers: 9 in numbers
